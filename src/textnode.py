22bad63df85cfc8670c96351b1e8673509b35974
chonginator <benchong1999@yahoo.com.au>
2024-05-23 09:55:54 +1000

    add text_type enum and types

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,19 @@
+from enum import Enum
+
+class TextType(Enum):
+  NORMAL = 1
+  BOLD = 2
+  ITALIC = 3
+  CODE = 4
+  LINK = 5
+  IMAGE = 6
 class TextNode:
-  def __init__(self, text, text_type, url=None):
+  def __init__(self, text: str, text_type: TextType, url: str | None=None):
     self.text = text
     self.text_type = text_type
     self.url = url
 
-  def __eq__(self, other):
+  def __eq__(self, other: "TextNode"):
     return (
       self.text == other.text
       and self.text_type == other.text_type
