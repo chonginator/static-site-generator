8b6eaa070f3381cfc19b4dae494a8b1303a6ec9d
chonginator <benchong1999@yahoo.com.au>
2024-05-24 13:32:48 +1000

    add image and link extraction functions

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,9 @@
 import unittest
+
 from inline_markdown import (
   split_nodes_delimiter,
+  extract_markdown_images,
+  extract_markdown_links,
 )
 
 from textnode import (
@@ -86,6 +89,23 @@ class TestInlineMarkdown(unittest.TestCase):
         new_nodes,
     )
 
+  def test_extract_markdown_images(self):
+    text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+    images = extract_markdown_images(text)
+
+    self.assertListEqual(images, [
+      ("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+      ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")
+    ])
+
+  def test_extract_markdown_links(self):
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    links = extract_markdown_links(text)
+
+    self.assertListEqual(links, [
+      ("link", "https://www.example.com"),
+      ("another", "https://www.example.com/another")
+    ])
 
 if __name__ == "__main__":
   unittest.main()
