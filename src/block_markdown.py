d059c562cc8e3522ef65a8187dfeb69e1218bb55
chonginator <benchong1999@yahoo.com.au>
2024-05-25 15:25:03 +1000

    add block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,12 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
   blocks = markdown.strip().split("\n\n")
   non_empty_blocks = list(filter(lambda block: block != "", blocks))
@@ -10,3 +19,43 @@ def markdown_to_blocks(markdown):
   )
 
   return stripped_non_empty_blocks
+
+def block_to_block_type(markdown_block):
+  if is_heading(markdown_block):
+    return block_type_heading
+  elif is_code_block(markdown_block):
+    return block_type_code
+  elif is_quote(markdown_block):
+    return block_type_quote
+  elif is_unordered_list(markdown_block):
+    return block_type_unordered_list
+  elif is_ordered_list(markdown_block):
+    return block_type_ordered_list
+  else:
+    print(f"block: {markdown_block}")
+    return block_type_paragraph
+
+def is_heading(markdown_block):
+  return re.match(r"^#{1,6} .+?$", markdown_block)
+
+def is_code_block(markdown_block):
+  lines = markdown_block.split("\n")
+  return (
+    len(lines) > 1
+    and lines[0].startswith("```")
+    and lines[-1].startswith("```")
+  )
+
+def is_quote(markdown_block):
+  lines = markdown_block.split("\n")
+  return all(re.match(r"^>.*?$", line) for line in lines)
+
+def is_unordered_list(markdown_block):
+  lines = markdown_block.split("\n")
+  return all(re.match(r"^[*-] .*?$", line) for line in lines)
+
+def is_ordered_list(markdown_block: str):
+  lines = markdown_block.split("\n")
+  return all(re.match(rf"^{i+1}. .*?$", line) for i, line in enumerate(lines))
+  
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,13 @@
 import unittest
 from block_markdown import (
+  block_type_paragraph,
+  block_type_heading,
+  block_type_code,
+  block_type_quote,
+  block_type_unordered_list,
+  block_type_ordered_list,
   markdown_to_blocks,
+  block_to_block_type,
 )
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -23,6 +30,30 @@ This is the same paragraph on a new line
   "* This is a list\n* with items"
       ]
     )
+
+  def test_block_to_block_type_heading(self):
+    block = "## This is a heading"
+    self.assertEqual(block_to_block_type(block), block_type_heading)
+
+  def test_block_to_block_type_code(self):
+    block = "```\nThis is a code block\n```"
+    self.assertEqual(block_to_block_type(block), block_type_code)
+
+  def test_block_to_block_type_quote(self):
+    block = "> This is a quote\n> This quote has two lines"
+    self.assertEqual(block_to_block_type(block), block_type_quote)
+
+  def test_block_to_block_type_unordered_list(self):
+    block = "* This is an unordered list\n- This is a bullet point"
+    self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+
+  def test_block_to_block_type_ordered_list(self):
+    block = "1. This is an ordered list\n2. This is my second point"
+    self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+
+  def test_block_to_block_type_paragraph(self):
+    block = "This is a plain paragraph.\nThis is another line in the paragraph"
+    self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
 if __name__ == "__main__":
   unittest.main()
